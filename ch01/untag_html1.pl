20ed1e888148e79c03b59a54cd56ceaf0afa9108
tomhickerson <tomh@xig.li>
2016-02-20 08:10:54 -0500

    Adding HTML parsing file, requires CPAN modules to run

---
+++ ch01/untag_html1.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use HTML::TreeBuilder;
+
+my $html = "<h1>What Junior Said</h1> <p>But I don't <font color='red'>want</font> to go to bed!</p>";
+
+my $tree = HTML::TreeBuilder->new;
+$tree->ignore_ignorable_whitespace(0);
+$tree->parse($html);
+$tree->eof();
+
+sub untag_html {
+    my ($html) = @_;
+    return $html unless ref $html;
+    # its a plain string
+    my $text = '';
+    for my $item (@{$html->{_content}}) {
+        $text .= untag_html($item);
+    }
+    return $text;
+}
+
+print untag_html($tree);
