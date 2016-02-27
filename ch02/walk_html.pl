fd7d4138030b99c8b195a6b07d0c4c48f5a1112e
tomhickerson <tomh@xig.li>
2016-02-27 04:36:14 -0500

    updates to walk_html exercise

--- ch02/walk_html.pl
+++ ch02/walk_html.pl
@@ -8,34 +8,51 @@ my @textarray;
 
 my $html = "<h1>loren ipsum</h1> <p>Neque porro quisquam</p> <p>est qui <font color='red'>dolorem ipsum</font></p>";
 
-my $tree = {{_tag=> "h1",
-            _content=> ["loren ipsum"]},
-            {_tag=> "p",
-            _content=> [ "Neque porro quisquam"]},
-            {_tag=> "p",
-             _content=> ["est qui", {_tag=> "font", _content=> ["dolorem ipsum"], color=> "red"}]}
+my $tree = {_tag=> "body",
+            _content=>
+                [{_tag=> "h1",
+                  _content=> ["loren ipsum"]},
+                 {_tag=> "p",
+                  _content=> [ "Neque porro quisquam"]},
+                 {_tag=> "p",
+                  _content=> ["est qui", {_tag=> "font", _content=> ["dolorem ipsum"], color=> "red"}]}]
 };
+
+my $tree2 = {};
 
 sub walk_html {
     my ($html, $textfunc, $elementfunc, $userparam) = @_;
     return $textfunc->($html, $userparam) unless ref $html;
     my ($item, @results);
+    #print "walking\n";
     for $item (@{$html->{_content}}) {
         push @results, walk_html($item, $textfunc, $elementfunc, $userparam);
+        #print Dumper(@results);
     }
     return $elementfunc->($html, $userparam, @results);
 }
 
 sub elementfunc {
-    my $table = { h1 => sub { shift; my $text3 = join '', @_; print $text3; return $text3; },
-                _DEFAULT_ => sub { shift; my $text4 = join '', @_; return $text4; }
+    my $table = { h1 => sub { print "H1\n"; return "H1"; },
+                _DEFAULT_ => sub { return "DEFAULT"; }
     };
 
-    my ($element) = @_;
+    my ($element, $userparam, $results) = @_;
     my $tag = $element->{_tag};
     my $action = $table->{$tag} || $table->{_DEFAULT_};
+    push @$userparam, $action->($element);
+    # note that we are pushing to the textarray referenced in the walk_html main code
     return $action->(@_);
 }
 
-@textarray = walk_html($tree, sub { my ($text, $aref) = @_; push @$aref, $text; }, \&elementfunc, \@textarray);
-print Dumper(\@textarray);
+# first run on walk_html with an elementfunc passed and the text array
+
+walk_html($tree, sub { my ($text, $aref) = @_; push @$aref, $text; }, \&elementfunc, \@textarray);
+print Dumper(@textarray);
+
+print "\n";
+
+# second run of the walk_html with no elementfunc, note that we add four lines on to the existing text array since we didn't clear it out
+
+walk_html($tree, sub { my ($text, $aref) = @_; push @$aref, $text; }, sub { }, \@textarray);
+print Dumper(@textarray);
