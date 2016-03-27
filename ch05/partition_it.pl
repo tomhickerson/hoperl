62266631b2d5a1d8d7b1a212cf5d7777626f2f2a
tomhickerson <tomh@xig.li>
2016-03-27 03:21:18 -0400

    adding some printlns to partition_it

--- ch05/partition_it.pl
+++ ch05/partition_it.pl
@@ -6,11 +6,14 @@ use Data::Dumper;
 
 sub make_partitioner {
     my ($n, $treasures) = @_;
+    print "@_\n";
     my @todo = [$n, $treasures, []];
     sub {
         while (@todo) {
+            print "@todo\n";
             my $cur = pop @todo;
             my ($target, $pool, $share) = @$cur;
+            print "$target \n";
             if ($target == 0) { return $share; }
             next if $target < 0 || @$pool == 0;
 
@@ -27,6 +30,6 @@ sub make_partitioner {
     } # end of anonymous function
 } # end of make_partitioner
 
-my @result = make_partitioner(4, [1,2,3,4]);
-
+my (@result, $share) = make_partitioner(4, [1,2,3,4]);
+print "$share\n";
 print Dumper(@result);
