67b043fb6d2b5671b60c1e00665388d8b1bd9bbf
tomhickerson <tomh@xig.li>
2016-04-18 03:45:15 -0400

    second try on sqrt_stream.pl from chapter 06

--- ch06/sqrt_stream.pl
+++ ch06/sqrt_stream.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-use Stream 'iterate_function';
+use Stream 'iterate_function', 'show', 'head';
 
 sub sqrt_stream {
     my $n = shift;
@@ -12,4 +12,12 @@ sub sqrt_stream {
 sub close_enough {
     my ($a, $b) = @_;
     return abs($a - $b) < 1e-12;
+}
+
+my $n = 2;
+my $g2 = $n;
+
+until (close_enough($g2*$g2, $n)) {
+    $g2 = head(sqrt_stream($n, $g2));
+    print "$g2\n";
 }
