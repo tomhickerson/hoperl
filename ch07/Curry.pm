2bd2b37146e7f8a96d9349650c0a4fcfe2324e81
tomhickerson <tomh@xig.li>
2016-05-05 03:15:56 -0400

    adding prototyping, curry_n to the Curry package

--- ch07/Curry.pm
+++ ch07/Curry.pm
@@ -2,16 +2,18 @@
 
 package Curry;
 use base 'Exporter';
+use Scalar::Util 'set_prototype';
 @EXPORT = ('curry');
 @EXPORT_OK = qw(curry_listfunc curry_n);
 
 sub curry {
     my $f = shift;
-    return sub {
+    my $PROTOTYPE = shift;
+    set_prototype(sub {
         my $first_arg = shift;
         my $r = sub { $f->($first_arg, @_)};
         return @_ ? $r->(@_) : $r;
-    };
+                  }, $PROTOTYPE);
 }
 
 sub curry_listfunc {
@@ -19,6 +21,20 @@ sub curry_listfunc {
     return sub {
         my $first_arg = shift;
         return sub { $f->($first_arg, @_)};
+    };
+}
+
+sub curry_n {
+    my $N = shift;
+    my $f = shift;
+    my $c;
+    $c = sub {
+        if (@_ >= $N) {
+            $f->(@_);
+        } else {
+            my @a = @_;
+            curry_n($N-@a, sub {$f->(@a, @_)});
+        }
     };
 }
 
