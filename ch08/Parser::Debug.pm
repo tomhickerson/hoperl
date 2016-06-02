9ccb1413b2d599a4bd81afad588c1564ce64cdfa
tomhickerson <tomh@xig.li>
2016-06-02 03:46:38 -0400

    adding code from debug subsection page 419-420 of Chapter 08

---
+++ ch08/Parser::Debug.pm
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+package Parser::Debug;
+
+use base 'Exporter';
+use Parser ':all';
+@EXPORT_OK = @Parser::EXPORT_OK;
+%EXPORT_TAGS = %Parser::EXPORT_TAGS;
+
+my $CON = 'A';
+
+sub concatenate {
+
+}
+
+sub debug ($) {
+    return unless $DEBUG || $ENV{$DEBUG};
+    my $msg = shift;
+    my $i = 0;
+    $i++ while caller($i);
+    $I = "| " x ($i-2);
+    print $I, $msg;
+}
