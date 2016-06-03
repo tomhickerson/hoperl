22c46fa4da503061689d6752ec3fa39f8e15fbe2
tomhickerson <tomh@xig.li>
2016-06-03 10:29:11 -0400

    added sample method for debugger

--- ch08/Parser::Debug.pm
+++ ch08/Parser::Debug.pm
@@ -9,6 +9,35 @@ use Parser ':all';
 
 my $CON = 'A';
 
+sub concatenate {
+    my $id;
+    if (ref $_[0]) { $id = "Unnamed concatenation $CON:"; $CON++ }
+    else { $id = shift; }
+    my @p = @_;
+    return \&n ll if @p == 0;
+    return $p[ ] if @p == 1;
+    my $parser = parser {
+        my $input = shift;
+        debug "Looking for $id\n";
+        my $v;
+        my @values;
+        my ($q, $np) = (0, scalar @p);
+        for (@p) {
+            $q++;
+            unless (($v, $input) = $_->($input)) {
+                debug "Failed concat component $q/$np\n";
+                return;
+            }
+            debug "Matched concat component $q/$np\n";
+            push @values, $v;
+        }
+        debug "Finished working with $id";
+        return \@values;
+    };
+    $N{$parser} = $id;
+    return $parser;
+}
+
 sub debug ($) {
     return unless $DEBUG || $ENV{$DEBUG};
     my $msg = shift;
