db532b879c9247dbbadfa39969774afd8715e30c
tomhickerson <tomh@xig.li>
2016-06-08 07:08:29 -0400

    unfinished pieces to a regex parser from chapter 08

---
+++ ch08/Regex.pm
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+# the Regex package, from chapter 06 of HOP
+
+package Regex;
+use Stream ':all';
+use base 'Exporter';
+@EXPORT_OK = qw(literal union concat star plus charclass show2 matches query);
+
+sub literal {
+    my $string = shift;
+    node($string, undef);
+}
+
+sub index_of_shortest {
+    my @s = @_;
+    my $minlen = length(head($s[0]));
+    my $si = 0;
+    for (1..$#s) {
+        my $h = head($s[$_]);
+        if (length($h) < $minlen) {
+            $minlen = length($h);
+            $si = $_;
+        }
+    }
+    $si;
+}
+
+sub union {
+    my (@s) = grep $_, @_;
+    return unless @s;
+    return $s[0] if @s == 1;
+    my $si = index_of_shortest(@s);
+    node(head($s[$si]), promise {
+        union(map $_ == $si ? tail($s[$_]) : $s[$_], 0..$#s) });
+}
+
+sub precat {
+    my ($s, $c) = @_;
+    transform { "$c$_[0]" } $s;
+}
+
+sub postcat {
+    my ($s, $c) = @_;
+    transform { "$_[0]$c" } $s;
+}
+
+sub concat {
+    my ($S, $T) = @_;
+    return unless $S && $T;
+    my ($s, $t) = (head($S), head($T));
+
+    node("$s$t", promise { union(postcat(tail($S), $t),
+                                 precat(tail($T), $s),
+                                 concat(tail($S), tail($T)))});
+}
+
+sub star {
+    my $s = shift;
+    my $r;
+    $r = node("", promise { concat($s, $r) });
+}
+
+# changing show to show2 to differentiate from Stream::show
+sub show2 {
+    my ($s, $n) = @_;
+    while ($s && (! defined $n || $n-- > 0)) {
+        print qq{"}, drop($s), qq{"\n};
+    }
+    print "\n";
+}
+
+# charclass('acd') = /^[acd]$/
+sub charclass {
+    my $class = shift;
+    union(map literal($_), split(//, $class));
+}
+
+# plus($s) = /^s+$/
+sub plus {
+    my $s = shift;
+    concat($s, star($s));
+}
+
+# added specifically for chapter 08
+sub query {
+    my $s = shift;
+    union(literal(""), $s);
+}
---
+++ ch08/regex-parser.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use Lexer ':all';
+use Stream 'node';
+
+my ($regex, $alternative, $atom, $qatom);
+sub regex_to_stream {
+    my @input = @_;
+    my $input = sub { shift @input };
+
+    my $lexer = iterator_to_stream(make_lexer($input,
+                                   ['ATOM', qr/\\x[0-9a-fA-F]{0,2}|\\\d+|\\./x,],
+                                   ['PAREN', qr/[()]/,],
+                                   ['QUANT', qr/[*+?]/ ],
+                                   ['BAR', qr/[|]/, ],
+                                   ['ATOM', qr/./, ]
+                                   )
+        );
+
+    my ($result) = $regex->($lexer);
+    return $result;
+}
+
+use Parser ':all';
+use Regex;
+
+my $Regex = parser { $regex->(@_)};
+my $Alternative = parser { $alternative->(@_)};
+my $Atom = parser { $atom->(@_)};
+my $QAtom = parser { $qatom->(@_)};
+
+$regex = alternate(T(concatenate($Alternative, lookfor('BAR'), $Regex), sub { Regex::union($_[0], $_[2])}), $Alternative);
+$alternative = alternate(T(concatenate($QAtom, $Alternative), \&Regex::concat), T(\&nothing, sub { Regex::literal("")}));
+my %quant;
+$qatom = T(concatenate($Atom, alternate(lookfor('QUANT'), \&nothing),
+           ),
+           sub { my($at, $q) = @_; defined $q ? $quant{$q}->($at) : $at });
+
+%quant = ('*' => \&Regex::star, '+' => \&Regex::plus, '?' => \&Regex::query);
