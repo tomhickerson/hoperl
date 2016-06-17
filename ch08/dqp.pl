993c7c9dcdb3d077b6dc9a95615babd1d2fdadbb
tomhickerson <tomh@xig.li>
2016-06-17 03:06:32 -0400

    adding on to data query language, from chapter 08

--- ch08/dqp.pl
+++ ch08/dqp.pl
@@ -38,3 +38,49 @@ my $Term = parser { $term->(@_) };
 use FlatDB;
 $cquery = operator($Term, [lookfor('OR'), \&query_or] );
 $term = operator($SQuery, [lookfor('AND'), \&query_and] );
+
+my $parser_dbh;
+sub set_parser_dbh { $parser_dbh = shift }
+sub parser_dbh { $parser_dbh }
+
+my %string_version = ('>' => 'gt', '>=' => 'ge', '==' => 'eq', '<' => 'lt', '<=' => 'le', '!=' => 'ne');
+
+$squery = alternate(T(concatenate(lookfor('LPAREN'), $CQuery, lookfor('RPAREN'),), sub { $_[1]}),
+    T(concatenate(lookfor('FIELD'), lookfor('OP'), lookfor('NUMBER')),
+      sub {
+          my ($field, $op, $val) = @_;
+          my $cmp_code = 'sub { $_[0] OP $_[1] }';
+          $cmp_code =~ s/OP/$op/;
+          my $cmp = eval($cmp_code) or die;
+          my $cb = sub { my %F = @_; $cmp->($F{$field}, $val)};
+          $parser_dbh->callbackquery($cb);
+      }),
+    T(concatenate(lookfor('FIELD'), lookfor('OP'), lookfor('STRING')),
+                    sub {
+                        if ($_[1] eq '==') {
+                            $parser_dbh->query($_[0], $_[2]);
+                        } else {
+                            my ($field, $op, $val) = @_;
+                            my $cmp_code = 'sub { $_[0] OP $_[1] }';
+                            $cmp_code =~ s/OP/$string_version{$op}/;
+                            my $cmp = eval($cmp_code) or die;
+                            my $cb = sub { my %F = @_; $cmp->($F{$field}, $val)};
+                            $parser_dbh->callbackquery($cb);
+                        }
+      }),
+    );
+
+sub parse_query {
+    my $self = shift;
+    my $query = shift;
+    my $lexer = lex_input($query);
+    my $old_parser_dbh = parser_dbh();
+    set_parser_dbh($self);
+    my ($result) = $cquery->($lexer);
+    set_parser_dbh($old_parser_dbh);
+    return $result;
+}
+
+use Data::Dumper;
+my $first_query = "STATE = 'NY' | OWES > 100 & STATE = 'MA'";
+print Dumper(parse_query($first_query));
