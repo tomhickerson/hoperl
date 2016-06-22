1774639c6503353eacddc2ac8443338e5ffd78a5
tomhickerson <tomh@xig.li>
2016-06-22 03:51:21 -0400

    adding to the behavior.pl file for HOP Chapter 09

--- ch09/behavior.pl
+++ ch09/behavior.pl
@@ -26,6 +26,60 @@ use Wire;
     };
     sub new_adder {
         my ($a1, $a2, $s) = @_;
-        Node->new('adder', $adder, { A1 => $a1, A2 => $a2, S => $s });
+        Node->new('Adder', $adder, { A1 => $a1, A2 => $a2, S => $s });
     }
+}
+
+{
+    my $multiplier = sub {
+        my ($self, %v) = @_;
+        if (defined $v{F1} && defined $v{F2}) {
+            $self->set_wire('P', $v{F1} * $v{F2});
+        } elsif (defined $v{F1} && $v{F1} == 0) {
+            $self->set_wire('P', 0);
+        } elsif (defined $v{F2} && $v{F2} == 0) {
+            $self->set_wire('P', 0);
+        } else {
+            $self->revoke_wire('P');
+        }
+        if (defined $v{F1} && defined $v{P}) {
+            if ($v{F1} != 0) {
+                $self->set_wire('F2', $v{P} / $v{F1});
+            } elsif ($v{P} != 0) {
+                warn "Division by zero\n";
+            }
+        } else {
+            $self->revoke_wire('F2');
+        }
+        if (defined $v{F2} && defined $v{P}) {
+            if ($v{F2} != 0) {
+                $self->set_wire('F1', $v{P} / $v{F2});
+            } elsif ($v{P} != 0) {
+                warn "Division by zero\n";
+            }
+        } else {
+            $self->revoke_wire('F1')
+        }
+    };
+    sub new_multiplier {
+        my ($f1, $f2, $p) = @_;
+        Node->new('Multiplier', $multiplier, { F1 => $f1, F2 => $f2, P => $p});
+    }
+}
+
+sub new_subtractor {
+    my ($s, $m, $d) = @_;
+    new_adder($d, $m, $s);
+}
+
+sub new_divider {
+    my ($v, $s, $q) = @_;
+    new_multiplier($q, $s, $v);
+}
+
+sub new_constant {
+    my ($val, $w) = @_;
+    my $node = Node->new('Constant', sub {}, {'W' => $w}, );
+    $w->set($node, $val);
+    $node;
 }
