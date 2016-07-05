5d26eefa8c9a8cca6ff8e01a0c0a564843dbcd73
tomhickerson <tomh@xig.li>
2016-07-05 03:28:10 -0400

    more Tuple for Chapter 09

--- ch09/Value/Tuple.pm
+++ ch09/Value/Tuple.pm
@@ -18,3 +18,35 @@ sub has_component { exists $_[0]{TUPLE}{$_[1]}}
 sub component { $_[0]{TUPLE}{$_[1]}}
 
 sub to_hash { $_[0]{TUPLE}}
+
+sub scale {
+    my ($self, $coeff) = @_;
+    my %new_tuple;
+    for my $k ($self->components) {
+        $new_tuple{$k} = $self->component($k)->scale($coeff);
+    }
+    $self->new(%new_tuple);
+}
+
+sub has_same_components_as {
+    my ($t1, $t2) = @_;
+    my %t1c;
+    for my $c ($t1->components) {
+        return unless $t2->has_component($c);
+        $t1c{$c} = 1;
+    }
+    for my $c ($t2->components) {
+        return unless $t1c{$c};
+    }
+    return 1;
+}
+
+sub add_tuples {
+    my ($t1, $t2) = @_;
+    croak("Nonconformable tuples") unless $t1->has_same_components_as($t2);
+    my %result;
+    for my $c ($t1->components) {
+        $result{$c} = $t1->component($c) + $t2->component($c);
+    }
+    $t1->new(%result);
+}
