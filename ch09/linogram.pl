676ddd1f93fa81985bb000017975a403781f9840
tomhickerson <tomh@xig.li>
2016-08-09 06:58:49 -0400

    more changes to linogram

--- ch09/linogram.pl
+++ ch09/linogram.pl
@@ -34,3 +34,10 @@ my $tokens = iterator_to_stream(make_lexer($input, @keywords,
 # the main data structure in linogram is Types. which is a hash mapping known Type names to the objects that represent them.
 my $ROOT_TYPE = Type->new('ROOT');
 my %TYPES = ('number' => Type::Scalar->new('number'), 'ROOT' => $ROOT_TYPE);
+
+# top level parser for our linogram
+$program = star($Definition | $Declaration
+    > sub { add_declarations($ROOT_TYPE, $_[0])}
+    ) - option($perl_code) - $End_of_input;
+
+$perl_code = _("ENDMARKER") > sub { eval $_[0]; die if $@; };
